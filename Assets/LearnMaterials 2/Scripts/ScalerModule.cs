40c7c285e45b9413fb60ac396142c84290e31866
yulia <juliajiyushkova@gmail.com>
2022-03-23 15:58:15 +0700

    ScalerModule

--- Assets/LearnMaterials 2/Scripts/ScalerModule.cs
+++ Assets/LearnMaterials 2/Scripts/ScalerModule.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1rdTEVSrCcYOjqTJcFCHj46RvnbdJhmQUb3gHMDhVftI/edit?usp=sharing")]
 public class ScalerModule : MonoBehaviour
 {
+    [Header("Модуль")]
+    [SerializeField]
+    private bool debug;
+
+    [SerializeField]
     private Vector3 targetScale = new Vector3(2,2,2);
 
+    [SerializeField]
+    [Min(1)]
     private float changeSpeed;
 
     private Vector3 defaultScale;
@@ -17,6 +24,11 @@ public class ScalerModule : MonoBehaviour
         myTransform = transform;
         defaultScale = myTransform.localScale;
         toDefault = false;
+
+        if (debug == true)
+        {
+            ActivateModule();
+        }
     }
 
     public void ActivateModule()
